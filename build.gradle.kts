08c9fa66a938f938a87e717595ccc297742ac128
GrowlyX <growlyxdev@gmail.com>
2023-11-12 13:48:44 -0500

    feat: the code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
 
-group = "gg.tropic.spa"
+group = "gg.tropic.banresets"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +15,23 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("org.litote.kmongo:kmongo:4.10.0")
+
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    exclude(
+        "**/*.kotlin_metadata",
+        "**/*.kotlin_builtins",
+        "META-INF/"
+    )
+
+    archiveFileName.set(
+        "scala-banresetter.jar"
+    )
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,38 @@
-fun main(args: Array<String>)
-{
-    println("Hello World!")
+import com.mongodb.client.MongoClients
+import com.mongodb.client.model.Filters
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import com.xenomachina.argparser.mainBody
+import org.litote.kmongo.withKMongo
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class BanResetsArgs(parser: ArgParser)
+{
+    val connectionUri by parser.storing("mongo connection uri")
+        .default("mongodb://localhost")
+
+    val database by parser.storing("mongo database name")
+    val collection by parser.storing("mongo collection name")
+        .default("Punishment")
+}
+
+fun main(args: Array<String>) = mainBody {
+    with(ArgParser(args).parseInto(::BanResetsArgs)) {
+        val client = MongoClients.create(connectionUri)
+        val database = client.getDatabase(database).withKMongo()
+        val collection = database.getCollection(collection).withKMongo()
+
+        with(
+            collection.deleteMany(
+                Filters.eq("category", "BAN")
+            )
+        ) {
+            if (wasAcknowledged())
+            {
+                println("Completed deletion of $deletedCount documents.")
+            } else
+            {
+                println("Failed to complete.")
+            }
+        }
+    }
 }
